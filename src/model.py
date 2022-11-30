75838cba2a6e508f5f9018baff6db4f3a07973ca
vadim <vadimlunev1908@gmail.com>
2022-11-30 19:09:21 +0500

    kak tokenbl delat

--- src/model.py
+++ src/model.py
@@ -10,3 +10,22 @@ from datetime import datetime
 class CountLettersResponse(BaseModel):
     counted_at: datetime
     counters: Dict[str, int]
+
+class NoteCreationResponse(BaseModel):
+    id: int
+
+class ReadNoteResponce(BaseModel):
+    id: int
+    text: str
+
+class GetNoteInfoResponse(BaseModel):
+    created_at: datetime
+    updated_at: datetime
+
+class UpdateNoteResponse(BaseModel):
+    id: int
+    old_text: str
+    new_text: str
+
+class ShowNotesResponse(BaseModel):
+    note_list: Dict[int, int]
--- src/web_app.py
+++ src/web_app.py
@@ -1,42 +1,59 @@
 import datetime
-import json
+from typing import Dict
 
 import fastapi
 
-from model import CountLettersResponse
+from model import CountLettersResponse, ReadNoteResponce, GetNoteInfoResponse, ShowNotesResponse, NoteCreationResponse, \
+    UpdateNoteResponse
+
+import os
 
 api_router = fastapi.APIRouter()
 
+notesDir = "Notes/"
 
-ID = 0
-def getId():
-    global ID
-    ID += 1
-    return ID - 1
+
+
 class Note:
+    content = ''
 
-    def __init__(self, content):
+    def __init__(self, id):
         self.createdAt = datetime.datetime.now()
         self.updatedAt = self.createdAt
-        self.id = getId()
-        self.content = content
+        self.id = id
+    def ChangeText(self, text):
+        content = text
+
+def commitNote(note: Note):
+    name = notesDir + str(note.id) + '.txt'
+    text_file = open(name, 'w')
+    text_file.write(note.content)
+
 notes = []
-def removeNote(id:int):
-    for i in range(notes):
-        if(notes[i].id == id):
+
+
+def removeNote(id: int):
+    for i in range(len(notes)):
+        if (notes[i].id == id):
             notes.pop(i)
+            return
+
+
 def getNoteById(id: int):
     global notes
     for note in notes:
-        if(note.id == id):
+        if (note.id == id):
             return note
     return -1;
+
+
 def getIds():
     global notes
     s = []
     for idx, note in enumerate(notes):
         s.append(str(idx) + ": " + str(note.id))
     return s;
+
 
 # Обрати внимание на @api_router.get <- get - тип HTTP метода!
 # Для того, чтобы создать put метод, необходимо написать @api_router.put
@@ -50,34 +67,65 @@ def sum_(a: float, b: float):
     """
     return a + b
 
-@api_router.post("/post", status_code = 201)
-def PostNote(a: str):
+
+@api_router.post("/post", response_model=NoteCreationResponse)
+def PostNote(a: int):
     note = Note(a)
     notes.append(note)
 
-@api_router.get("/get", response_model=json)
+    commitNote(note)
+
+    return NoteCreationResponse(
+        id=str(note.id),
+    )
+
+
+@api_router.get("/get", response_model=ReadNoteResponce)
 def GetNote(a: int):
     note = getNoteById(a)
-    return json.dumps({'id': str(note.id), 'text': str(note.content)})
+    return ReadNoteResponce(
+        id=str(note.id),
+        text=note.content
+    )
 
-@api_router.get("/getInfo", response_model=json)
+
+@api_router.get("/getInfo", response_model=GetNoteInfoResponse)
 def GetInfo(a: int):
     note = getNoteById(a)
-    return json.dumps({'created_at': str(note.createdAt), 'updated_at': str(note.updatedAt)})
+    return GetNoteInfoResponse(
+        created_at=note.createdAt,
+        updated_at=note.updatedAt
+    )
 
-@api_router.get("/GetNotesIds", response_model=json)
+
+@api_router.get("/GetNotesIds", response_model=ShowNotesResponse)
 def GetIds():
-    return json.dumps(getIds())
+    d = {}
+    for idx, i in enumerate(notes):
+        d.update({idx : i.id})
+    return ShowNotesResponse(
+        note_list=d
+    )
 
-@api_router.delete("/DeleteNote", status_code = 204)
+
+@api_router.delete("/DeleteNote", status_code= 204)
 def DeleteNoteById(id: int):
     removeNote(id)
 
-@api_router.patch("/changeNote", status_code = 201)
+
+@api_router.patch("/changeNote", response_model=UpdateNoteResponse)
 def ChangeNote(id: int, Text: str):
     note = getNoteById(id)
+    oldText = note.content
     note.content = Text
     note.updatedAt = datetime.datetime.now()
+    commitNote(note)
+    return UpdateNoteResponse(
+        id=str(note.id),
+        old_text=oldText,
+        new_text=note.content
+    )
+
 
 # Обрати внимание на response_model <- описывает схему ответа сервера
 @api_router.get("/count_letters", response_model=CountLettersResponse)
